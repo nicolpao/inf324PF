2a334ef4a60ef3f2c6efc6ef69925e1f968be704
nicolpao <91867241+nicolpao@users.noreply.github.com>
2021-12-06 02:48:44 -0400

    Respuestas de Examen Final

---
+++ Preg3-INF-324/WindowsFormsApplication21/WindowsFormsApplication21/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApplication21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(535, 419);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(553, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Recuperar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(588, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(588, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(588, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(567, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(567, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(567, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(694, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(535, 419);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(553, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Copiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(553, 148);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(135, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Recuperar ROJO";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(553, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Recuperar AMARILLO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(553, 206);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(135, 23);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "Recuperar VERDE";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(553, 235);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(135, 23);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "Guardar";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 449);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Preg3-INF-324/WindowsFormsApplication21/WindowsFormsApplication21/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication21
+{
+    public partial class Form1 : Form
+    {
+        int cR, cG, cB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//RECUPERAR
+            openFileDialog1.ShowDialog();
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            int x, y, mR=0, mG=0, mB=0;
+            x = e.X; y = e.Y;
+            for (int i = x; i < x + 10;i++)
+                for (int j = y; j < y + 10; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    mR = mR + c.R;
+                    mG = mG + c.G;
+                    mB = mB + c.B;
+                }
+            mR = mR / 100;
+            mG = mG / 100;
+            mB = mB / 100;
+            cR = mR;
+            cG = mG;
+            cB = mB;
+            textBox1.Text = cR.ToString();
+            textBox2.Text = cG.ToString();
+            textBox3.Text = cB.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {//COPIAR
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 1; i < bmp.Width; i++)
+                for (int j = 1; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    cpoa.SetPixel(i, j, c);
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {//**RECUPERAR ROJO**
+            //MUESTRA LO OBTENIDO DE LA BD
+            SqlConnection con = new SqlConnection();
+            SqlDataAdapter ada = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            con.ConnectionString = "server=(local);user=usuario324;pwd=123456;database=Imagenes";
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = "SELECT * FROM textura";
+            ada.Fill(ds);
+            DataRow row = ds.Tables[0].Rows[1];
+            String t1 = ds.Tables[0].Rows[1]["text1"].ToString();
+            String t2 = ds.Tables[0].Rows[1]["text2"].ToString();
+            String t3 = ds.Tables[0].Rows[1]["text3"].ToString();
+
+            int t11 = Int32.Parse(t1);
+            int t22 = Int32.Parse(t2);
+            int t33 = Int32.Parse(t3);
+
+            int meR, meG, meB;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width-10; i+=10)
+                for (int j = 0; j < bmp.Height-10; j+=10)
+                {
+                    meR = 0;
+                    meG = 0;
+                    meB = 0;
+
+                    for (int k = i; k < i+10; k++)
+                        for (int l = j; l < j + 10; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            meR = meR+c.R;
+                            meG = meG+c.G;
+                            meB = meB+c.B;
+                        }
+                    meR = meR /100;
+                    meG = meG /100;
+                    meB = meB /100;
+
+                    meR = t11;
+                    meG = t22;
+                    meB = t33;
+                    
+                    if (((cR - 10) < meR) && (meR < (cR + 10)) && ((cG - 10) < meG) && (meG < (cG + 10)) && ((cB - 10) < meB) && (meB < (cB + 10)))
+                        for (int k = i; k < i+10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                cpoa.SetPixel(k, l, Color.Red);
+                            }
+                        
+                    else
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                cpoa.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//**RECUPERAR AMARILLO**
+            //MUESTRA LO OBTENIDO DE LA BD
+            SqlConnection con = new SqlConnection();
+            SqlDataAdapter ada = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            con.ConnectionString = "server=(local);user=usuario324;pwd=123456;database=Imagenes";
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = "SELECT * FROM textura";
+            ada.Fill(ds);
+            DataRow row = ds.Tables[0].Rows[2];
+            String t1 = ds.Tables[0].Rows[2]["text1"].ToString();
+            String t2 = ds.Tables[0].Rows[2]["text2"].ToString();
+            String t3 = ds.Tables[0].Rows[2]["text3"].ToString();
+
+            int t11 = Int32.Parse(t1);
+            int t22 = Int32.Parse(t2);
+            int t33 = Int32.Parse(t3);
+
+            int meR, meG, meB;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 10; i += 10)
+                for (int j = 0; j < bmp.Height - 10; j += 10)
+                {
+                    meR = 0;
+                    meG = 0;
+                    meB = 0;
+
+                    for (int k = i; k < i + 10; k++)
+                        for (int l = j; l < j + 10; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            meR = meR + c.R;
+                            meG = meG + c.G;
+                            meB = meB + c.B;
+                        }
+                    meR = meR / 100;
+                    meG = meG / 100;
+                    meB = meB / 100;
+
+                    meR = t11;
+                    meG = t22;
+                    meB = t33;
+
+                    if (((cR - 10) < meR) && (meR < (cR + 10)) && ((cG - 10) < meG) && (meG < (cG + 10)) && ((cB - 10) < meB) && (meB < (cB + 10)))
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                cpoa.SetPixel(k, l, Color.Yellow);
+                            }
+
+                    else
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                cpoa.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {//**RECUPERAR VERDE**
+            //MUESTRA LO OBTENIDO DE LA BD
+            SqlConnection con = new SqlConnection();
+            SqlDataAdapter ada = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            con.ConnectionString = "server=(local);user=usuario324;pwd=123456;database=Imagenes";
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = "SELECT * FROM textura";
+            ada.Fill(ds);
+            DataRow row = ds.Tables[0].Rows[3];
+            String t1 = ds.Tables[0].Rows[3]["text1"].ToString();
+            String t2 = ds.Tables[0].Rows[3]["text2"].ToString();
+            String t3 = ds.Tables[0].Rows[3]["text3"].ToString();
+
+            int t11 = Int32.Parse(t1);
+            int t22 = Int32.Parse(t2);
+            int t33 = Int32.Parse(t3);
+
+            int meR, meG, meB;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 10; i += 10)
+                for (int j = 0; j < bmp.Height - 10; j += 10)
+                {
+                    meR = 0;
+                    meG = 0;
+                    meB = 0;
+
+                    for (int k = i; k < i + 10; k++)
+                        for (int l = j; l < j + 10; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            meR = meR + c.R;
+                            meG = meG + c.G;
+                            meB = meB + c.B;
+                        }
+                    meR = meR / 100;
+                    meG = meG / 100;
+                    meB = meB / 100;
+
+                    meR = t11;
+                    meG = t22;
+                    meB = t33;
+
+                    if (((cR - 10) < meR) && (meR < (cR + 10)) && ((cG - 10) < meG) && (meG < (cG + 10)) && ((cB - 10) < meB) && (meB < (cB + 10)))
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                cpoa.SetPixel(k, l, Color.Green);
+                            }
+
+                    else
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                cpoa.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {//GUARDAR
+            SqlConnection con = new SqlConnection();
+            SqlDataAdapter ada = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            con.ConnectionString = "server=(local);user=usuario324;password=123456;database=Imagenes";
+            cmd.CommandText = "insert into textura(id_img,text1,text2,text3) values('','" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+    }
+}
